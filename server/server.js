2aacad4c4193c3f878e1f2fd7ae7492b4927c150
agolomazov <agolomazov@4xxi.com>
2018-05-17 14:25:53 +0300

    Add connect and disconnect status into socket

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', socket => {
+  console.log('connect user', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('disconnect user from server');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
