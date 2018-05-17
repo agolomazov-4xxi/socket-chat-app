01e11371f5177f9f3e024ba63b6c14406f7d73bf
agolomazov <agolomazov@4xxi.com>
2018-05-17 19:21:12 +0300

    Add geolocation support

--- public/js/index.js
+++ public/js/index.js
@@ -28,3 +28,22 @@ jQuery('#message-form').on('submit', function(e) {
   );
   input.val('');
 });
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+  if (!navigator.geolocation) {
+    return console.log('Geolocation not supported by your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition(
+    function(position) {
+      socket.emit('createLocationMessage', {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    },
+    function() {
+      console.log('Unable to fetch location');
+    }
+  );
+});
--- server/server.js
+++ server/server.js
@@ -34,6 +34,13 @@ io.on('connection', socket => {
     callback('This is from the server');
   });
 
+  socket.on('createLocationMessage', coords => {
+    io.emit(
+      'newMessage',
+      generateMessage('Admin', `${coords.latitude}, ${coords.longitude}`)
+    );
+  });
+
   socket.on('disconnect', () => {
     console.log('disconnect user from server');
   });
